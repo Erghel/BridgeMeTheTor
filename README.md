2336f94039c6e1394b80ea686a27f9bf33e07de7
Erghel <88831850+Erghel@users.noreply.github.com>
2022-05-02 15:13:53 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
 <p align="center">
-  <img src="images/logo.png" alt="Logo"></img>
-  
-  <br/>
-  <sub>Logo by <a href="https://github.com/lencodigitexer">@lencodigitexer</a></sub>
+  <img src="images/tor.png" alt="Logo"></img>
 </p>
-
-<h1 align="center">BridgeMeTheTor</h1>
-
-<h2> Ru </h2>
-  <p> Копия <b><a href="https://torscan-ru.ntc.party/">сайта</a></b> (его можно скачать и развернуть локально, в репозитории есть копия сайта, если захотите скачать), который создал <b><a href="https://twitter.com/ValdikSS">ValdikSS</a></b>, переделанное в расширение для браузера, оно помогает искать рабочие мосты для подключению к <b>Tor</b>.
-  Расширение, которое позволило бы использовать сайт, как расширение для браузера, сначала идея казалась хорошей, но пообщавшись с разработчиком сайта <b>мы решили закрыть проект</b> (то есть не публиковать его), а общими усилиями создать новое решение позволяющее людям искать и использовать мосты для <b>Tor</b>. Вы можете клонировать репозиторий на компьютер и <b>использовать расширение самостоятельно</b>.</p> 
-
-  <br/>
-<h2> Eng </h2>
-  <p> Copy <b><a href="https://torscan-ru.ntc.party /">of the site</a></b> (it can be downloaded and deployed locally, there is a copy of the site in the repository if you want to download it) that created <b><a href="https://twitter.com/ValdikSS">ValdikSS</a></b>, converted into a browser extension, it helps to search for working bridges to connect to <b>Tor</b>.
-An extension that would allow you to use the site as a browser extension, at first the idea seemed good, but after talking with the developer of the site <b>we decided to close the project </b> (that is, not publish it), and jointly create a new solution that allows people to search and use bridges for <b>Tor</b>. You can clone the repository to a computer and <b>use the extension yourself</b>.</p>
-
-  <br/>
-<h2> Credit </h2>
-<p>Icons made by:
-  <ul>
-<li> <a href="https://www.flaticon.com/free-icons/internet" title="internet icons">Internet icons created by Freepik - Flaticon</a> </li>
-    <li> <a href="https://iconscout.com/icons">Iconscount</a> </li>
-  </ul>
-
-The API is used - <a href="https://onionoo.torproject.org/details?type=relay&running=true&fields=fingerprint,or_addresses" title="internet icons">onionoo.torproject.org</a>
--- script.js
+++ script.js
@@ -1,104 +1,51 @@
-        // register service worker for PWA/offline cache
-        window.addEventListener("load", () => {
-            if ("serviceWorker" in navigator) {
-                navigator.serviceWorker.register("service-worker.js");
-            }
-        });
+window.addEventListener("load", () => {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("service-worker.js");
+    }
+});
 
-        const ONIONOO_URL = "https://onionoo.torproject.org/details?type=relay&running=true&fields=fingerprint,or_addresses";
+const LIST_URL = "https://torscan-ru.ntc.party/relays.txt";
 
-        const fetchWithTimeout = (uri, options = {}, time = 10000) => {
-            const controller = new AbortController();
-            const config = { ...options, signal: controller.signal };
+const gettor = () => {
+    return fetch(LIST_URL);
+};
 
-            const timeout = setTimeout(() => {
-                controller.abort()
-            }, time);
+const gettor_corsproxy1 = () => {
+    return fetch(`https://corsanywhere.herokuapp.com/${LIST_URL}`);
+};
 
-            return fetch(uri, config);
-        };
+const gettor_corsproxy2 = () => {
+    return fetch(`https://tauron.herokuapp.com/${LIST_URL}`);
+};
 
-        const check_port = (hostport) => {
-            // from https://searchfox.org/mozilla-central/source/netwerk/base/nsIOService.cpp
-            let badports = [1, 7, 9, 11, 13, 15, 17, 19, 20, 21, 22, 23, 25, 37, 42, 43, 53, 69, 77, 79, 87,
-                95, 101, 102, 103, 104, 109, 110, 111, 113, 115, 117, 119, 123, 135, 137, 139, 143, 161, 179,
-                389, 427, 465, 512, 513, 514, 515, 526, 530, 531, 532, 540, 548, 554, 556, 563, 587, 601, 636,
-                989, 990, 993, 995, 1719, 1720, 1723, 2049, 3659, 4045, 5060, 5061, 6000, 6566, 6665, 6666,
-                6667, 6668, 6669, 6697, 10080];
+const gettor_corsproxy3 = () => {
+    return fetch(`https://corsbypasser.herokuapp.com/${LIST_URL}`);
+};
 
-            let url = new URL(`https://${hostport}`);
+const process_data = (result) => {
+    document.getElementById("working").value = result;
+};
 
-            return !badports.includes(parseInt(url.port));
-        };
-
-        const check = (hostport, fingerprint) => {
-            fetchWithTimeout(`https://${hostport}`)
-                .then((response) => document.getElementById("result").innerText = response)
-                .catch((error) => {
-                    const line = `${hostport} ${fingerprint}\n`;
-
-                    if (error.name === 'AbortError') {
-                        document.getElementById("notworking").value += line;
-                    } else {
-                        document.getElementById("working").value += line;
-                    }
-                })
-        };
-
-        const gettor = () => {
-            return fetch(ONIONOO_URL);
-        };
-
-        const gettor_corsproxy1 = () => {
-            return fetch(`https://corsbypasser.herokuapp.com/${ONIONOO_URL}`);
-        };
-
-        const gettor_corsproxy2 = () => {
-            return fetch(`https://corsanywhere.herokuapp.com/${ONIONOO_URL}`);
-        };
-
-        const gettor_corsproxy3 = () => {
-            return fetch(`https://tauron.herokuapp.com/${ONIONOO_URL}`);
-        };
-
-        const process_data = (result) => {
-            let relays = result.relays;
-            let new_relays = [];
-
-            for (let i = 0; i < 30; i++) {
-                new_relays.push(relays[Math.floor(Math.random() * relays.length)]);
-            }
-
-            for (let relay of new_relays) {
-                for (let address of relay.or_addresses) {
-                    if (address.search("\\[") === -1 && check_port(address)) {
-                        // skip ipv6 because js can't detect unavailability reason
-                        check(address, relay.fingerprint);
-                    }
-                }
-            }
-        };
-
-        const do_everything = (datafunc) => {
-            return datafunc().then(result => {
-                return result.json();
-            }).then(result => {
-                process_data(result);
-            });
-        };
+const do_everything = (datafunc) => {
+    return datafunc().then(result => {
+        return result.text();
+    }).then(result => {
+        process_data(result);
+    });
+};
 
 
-        do_everything(gettor)
+do_everything(gettor)
+    .catch(error => {
+        console.log(error);
+
+        do_everything(gettor_corsproxy1)
             .catch(error => {
                 console.log(error);
-
-                do_everything(gettor_corsproxy1)
+                do_everything(gettor_corsproxy2)
                     .catch(error => {
                         console.log(error);
-                        do_everything(gettor_corsproxy2)
-                            .catch(error => {
-                                console.log(error);
-                                do_everything(gettor_corsproxy3);
-                            });
+                        do_everything(gettor_corsproxy3);
                     });
             });
+    });
